7038e8933d6faf9929bbc1d2b7b7571b591d6d79
Ramkumar R <indianairforceall@gmail.com>
2024-05-10 13:14:18 +0000

    IP added

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
+const requestIp = 'request-ip';
 
+app.use(requestIp.mw());
 app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
+  const username = req.query.username || 'rambozindia';
   try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+    // const result = await axios.get(
+    //   `https://api.github.com/users/${username}/repos`
+    // );
+    // const repos = result.data
+    //   .map((repo) => ({
+    //     name: repo.name,
+    //     url: repo.html_url,
+    //     description: repo.description,
+    //     stars: repo.stargazers_count
+    //   }))
+    //   .sort((a, b) => b.stars - a.stars);
 
-    res.send(repos);
+    const clientIp = req.clientIp;
+    const userAgent = req.headers['user-agent'];
+
+    res.send(`Client IP Address: ${clientIp}<br>User Agent: ${userAgent}`);
+
+    // res.send(repos);
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
   }
